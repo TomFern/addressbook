e8b47a86a384f7c971c58788fb10cadbd19d9ce9
tomas f <46322567+TomFern@users.noreply.github.com>
2025-11-13 21:56:18 +0000

    use sqlite for tests

--- database.js
+++ database.js
@@ -1,15 +1,23 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres',
-                                process.env.DB_USER || 'postgres',
-                                process.env.DB_PASSWORD || '',
-                                {
-                                    host: process.env.DB_HOST || 'localhost',
-                                    port: process.env.DB_PORT || 5432,
-                                    dialect: 'postgres',
-                                    dialectOptions: {
-                                        ssl: process.env.DB_SSL == "true"
-                                    }
-                                });
+const isTestEnv = process.env.NODE_ENV === 'test';
+
+const sequelize = isTestEnv
+    ? new Sequelize({
+        dialect: 'sqlite',
+        storage: ':memory:',
+        logging: false
+    })
+    : new Sequelize(process.env.DB_SCHEMA || 'postgres',
+        process.env.DB_USER || 'postgres',
+        process.env.DB_PASSWORD || '',
+        {
+            host: process.env.DB_HOST || 'localhost',
+            port: process.env.DB_PORT || 5432,
+            dialect: 'postgres',
+            dialectOptions: {
+                ssl: process.env.DB_SSL == "true"
+            }
+        });
 const Person = sequelize.define('Person', {
     firstName: {
         type: Sequelize.STRING,
